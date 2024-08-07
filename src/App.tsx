37a10b78060febe0044ed80c633ab608d65e65d8
avayyyyyyy <jabhi465@gmail.com>
2024-08-07 18:56:13 +0530

    fix: decrease duration of toaster component to 4000ms in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { Toaster } from "./components/sonner";
 function App() {
   return (
     <div className="fixed bottom-6 right-6">
-      <Toaster richColors duration={5000} closeButton />
+      <Toaster richColors duration={4000} closeButton />
       <CustomWidget projectid="clzjcvzw8001gh3axuwaszzgz" />
     </div>
   );
